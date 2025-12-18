9ed3509baa0b8190fc9d140a6a6300ff01e7dd17
szymon <szymon.pt4sznik@gmail.com>
2025-12-18 14:47:17 +0100

    init function for iteration methods

--- lab7.cpp
+++ lab7.cpp
@@ -20,6 +20,8 @@ double x0[N]={6, 6, 6, 6, 6};
 
 double L[N][N] = {}, D[N][N] = {}, U[N][N] = {};
 
+double o = 0.5;
+
 void print_matrix(double M[][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -56,6 +58,14 @@ void mult_mm(double B1[][N], double B2[][N], double R[][N]) {
     }
 }
 
+void mult_ms(double B1[N][N], double s, double R[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            R[i][j] = s * B1[i][j];
+        }
+    }
+}
+
 void mult_mv(double B[][N], double v[N], double r[N]) {
     for (int i = 0; i < N; i++) {
         r[i] = 0.0;
@@ -75,18 +85,6 @@ void decompose_A() {
     }
 }
 
-void Jacoby_method(double Jm[][N], double Jc[N], double Jx[N], double Jxn[N]) {
-    cout << "Jacoby" << endl;
-}
-
-void Gauss_Seidel_method(double Gm[][N], double Gc[N], double Gx[N], double Gxn[N]) {
-    cout << "Gauss-Seidel" << endl;
-}
-
-void SOR_method(double Sm[][N], double Sc[N], double Sx[N], double Sxn[N]) {
-    cout << "SOR" << endl;
-}
-
 void init_J(double Jm[][N], double Jc[N], double Jx[N], double Jxn[N]) {
     double Dr[N][N] = {};
     double mDr[N][N] = {};
@@ -99,22 +97,51 @@ void init_J(double Jm[][N], double Jc[N], double Jx[N], double Jxn[N]) {
     sum_mm(L, U, LU);
     mult_mv(Dr, b, Jc);  // wektor C
     mult_mm(mDr, LU, Jm);  // macierz M
+
     copy(x0, x0 + N, Jx);
     fill(Jxn, Jxn + N, 0.0);
 }
 
-void init_G(double Gm[][N], double Gc[N], double Gx[N], double Gxn[N]) {
+void init_G(double GL[][N], double GU[][N], double Gx[N], double Gxn[N]) {
+    mult_ms(U, -1., GU);
+    sum_mm(L, D, GL);  // L + D
 
+    copy(x0, x0 + N, Gx);
+    fill(Gxn, Gxn + N, 0.0);
 }
 
-void init_S(double Sm[][N], double Sc[N], double Sx[N], double Sxn[N]) {
+void init_S(double SL[][N], double SU[N][N], double Sx[N], double Sxn[N]) {
+    // L + 1/o * D
+    double oD1[N][N] = {};
+    mult_ms(D, (1. / o), oD1);
+    sum_mm(L, oD1, SL);
 
+    // - [(1 - o) * D + U]
+    double Su[N][N] = {};
+    double oD2[N][N] = {};
+    mult_ms(D, (1. - (1. / o)), oD2);
+    sum_mm(oD2, U, Su);
+    mult_ms(Su, -1, SU);
+
+    copy(x0, x0 + N, Sx);
+    fill(Sxn, Sxn + N, 0.0);
+}
+
+void Jacoby_method(double Jm[][N], double Jc[N], double Jx[N], double Jxn[N]) {
+    cout << "Jacoby" << endl;
+}
+
+void Gauss_Seidel_method(double Gm[][N], double Gc[N], double Gx[N], double Gxn[N]) {
+    cout << "Gauss-Seidel" << endl;
+}
+
+void SOR_method(double Sm[][N], double Sc[N], double Sx[N], double Sxn[N]) {
+    cout << "SOR" << endl;
 }
 
 int main() {
-    double Jm[N][N], Gm[N][N], Sm[N][N];
-    double Jc[N], Gc[N], Sc[N];
-    double Jx[N], Gx[N], Sx[N];
+    double Jm[N][N], GL[N][N], GU[N][N], SL[N][N], SU[N][N];
+    double Jc[N], Jx[N], Gx[N], Sx[N], Jxn[N], Gxn[N], Sxn[N];
 
     decompose_A();
 }
